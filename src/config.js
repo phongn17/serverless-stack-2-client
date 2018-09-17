1e45beef604e783c26cb3f24a8f3949b7b6a8c55
Phonhg Nguyen <phongnguyen@Phonhgs-MBP.frontierlocal.net>
2018-09-17 08:33:09 -0700

    adding stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,35 +1,37 @@
 const dev = {
   s3: {
-    REGION: "YOUR_DEV_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1e9wbmmiyuxy7"
   },
   apiGateway: {
-    REGION: "YOUR_DEV_API_GATEWAY_REGION",
-    URL: "YOUR_DEV_API_GATEWAY_URL"
+    REGION: "us-east-1",
+    URL: "https://pmwz5uykba.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "YOUR_DEV_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_DEV_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_DEV_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_DEV_IDENTITY_POOL_ID"
-  }
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_t97bCEzT1",
+    APP_CLIENT_ID: "6oiv1birgt18qjptie06dcsma3",
+    IDENTITY_POOL_ID: "us-east-1:af8c687c-07fc-43bc-be87-da176d8d8d49"
+  },
+  stripeKey: "pk_test_cPOJibF7ZbFCk0QKbP72iQLx"
 };
 
 const prod = {
   s3: {
-    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    REGION: "us-east-1",
     BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "YOUR_PROD_API_GATEWAY_REGION",
+    REGION: "us-east-1",
     URL: "YOUR_PROD_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: "YOUR_PROD_COGNITO_REGION",
+    REGION: "us-east-1",
     USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
     APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
     IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
-  }
+  },
+  stripeKey: "pk_test_cPOJibF7ZbFCk0QKbP72iQLx"
 };
 
 // Default to dev if not set
